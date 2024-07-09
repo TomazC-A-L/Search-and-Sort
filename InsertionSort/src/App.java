b5ac7a770d5a725181eea392d74dd326a9d7596d
TomazC-A-L <165029017+TomazC-A-L@users.noreply.github.com>
2024-07-08 22:11:25 -0300

    ading the celula item and main method

--- InsertionSort/src/App.java
+++ InsertionSort/src/App.java
@@ -1,8 +1,11 @@
-import java.util.*;
 public class App {
-    public static void main(String[] args){
-        Ordenador.Inicializador();
-        Ordenador.printArray();
-        Ordenador.insertionSort();
+    public static void main(String[] args) throws Exception {
+        ListaSE lista = new ListaSE();
+        lista.inserirNoInicio(new Celula(10));
+        lista.inserirNoInicio(new Celula(20));
+        lista.inserirNoFinal(new Celula(40));
+        lista.inserirNoFinal(new Celula(50));
+        lista.inserirNoMeio(new Celula(30));
+        lista.imprimir();
     }
 }
---
+++ InsertionSort/src/Celula.java
@@ -0,0 +1,8 @@
+public class Celula {
+        int valor;
+        Celula prox;
+
+        public Celula(int valor){
+             this.valor = valor;
+        }
+}
