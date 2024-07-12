010573f7390debdf1b369eea8ee22b8e8c6441ff
TomazC-A-L <totodelima47@gmail.com>
2024-07-11 22:04:17 -0300

    Commit Lista encadeada

--- InsertionSort/src/App.java
+++ InsertionSort/src/App.java
@@ -1,11 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
         ListaSE lista = new ListaSE();
-        lista.inserirNoInicio(new Celula(10));
-        lista.inserirNoInicio(new Celula(20));
-        lista.inserirNoFinal(new Celula(40));
-        lista.inserirNoFinal(new Celula(50));
-        lista.inserirNoMeio(new Celula(30));
+
+        Celula a = new Celula(10);
+        Celula b = new Celula(20);
+        Celula c = new Celula(30);
+        Celula d = new Celula(40);
+        Celula e = new Celula(60);
+
+        lista.inserirNoInicio(a);
+        lista.inserirNoFinal(b);
+        lista.inserirNoFinal(c);
+        lista.inserirNoFinal(d);
+        lista.inserirNoMeio(e);
+        lista.removerIndex(50);
+
         lista.imprimir();
+        //System.out.println(lista.pesquisar(20));
     }
 }
--- InsertionSort/src/ListaSE.java
+++ InsertionSort/src/ListaSE.java
@@ -5,14 +5,18 @@ public class ListaSE {
         primeiro = null;
     }
 
+    public boolean vazia(){
+        return (primeiro == null);
+    }
+
     public void inserirNoInicio(Celula c) {
         c.prox = primeiro;
         primeiro = c;
     }
 
     public void inserirNoFinal(Celula c){
-         if(c == null) {
-            
+         if(vazia()) {
+            primeiro = c;
          } else {
             Celula aux = primeiro;
             while(aux.prox != null) {
@@ -24,33 +28,96 @@ public class ListaSE {
 
     public void inserirNoMeio(Celula c){
         Celula aux = primeiro;
-        if(aux == null){
+        if(vazia()){
+            primeiro = c;
+        }else if (aux.prox == null) {
             if(aux.valor > c.valor){
-                inserirNoInicio(c);
+                inserirNoFinal(c);
             }
             if(aux.valor < c.valor) {
+                c.prox = primeiro;
+                primeiro = c;
+            }
+        }else {
+            while(aux.prox != null) {
+                if(c.valor > aux.valor && c.valor < aux.prox.valor){
+                    c.prox = aux.prox;
+                    aux.prox = c;
+                }
+                aux = aux.prox; 
+            }
+            if(aux.prox == null){
                 inserirNoFinal(c);
             }
         }
-        while(aux.prox != null) {
-            if(c.valor > aux.valor && c.valor < aux.prox.valor){
-                c.prox = aux.prox;
-                aux.prox = c;
+    }
+
+    public Celula pesquisar(int v) {
+        if (vazia()){
+            return null;
+        } else {
+            Celula aux = primeiro;
+            while(aux.valor != v && aux != null) {
+                aux = aux.prox;
             }
-            if(aux.prox == null){
-                aux.prox = c;
-            }
-            aux = aux.prox; 
+            return aux;
         }
     }
 
-    public void removerNoFinal(Celula c){
-        Celula aux = primeiro;
-        while(aux.prox.prox != null){
-            aux = aux.prox;
+    public void removeInicio(){
+        if(vazia()){
+            return;
+        } else {
+            Celula aux = primeiro;
+            if(aux.prox == null) {
+                primeiro = null;
+            } else{
+                primeiro = aux.prox;
+                aux.prox = null;
+            }
         }
-        aux.prox = null;
-        c = null;
+    }
+
+    public void removerNoFinal(){
+        if(vazia()){
+            return;
+        }
+        else {
+            Celula aux = primeiro;
+            if(aux.prox != null){
+                while(aux.prox.prox != null){
+                    aux = aux.prox;
+                }
+            aux.prox = null;
+            } else {
+                aux = primeiro;
+                primeiro = null;
+            }
+        }
+    }
+
+    public void removerIndex(int valor){
+        if (vazia()){
+            return;
+        }
+        else {
+            Celula aux = primeiro;
+            if(aux.valor == valor) {
+                removeInicio();
+            } else {
+                while(aux.prox != null){
+                    if(aux.prox.valor != valor) {
+                        aux = aux.prox;
+                    } else {
+                        break;
+                    }
+                }
+                if (aux.prox != null && aux.prox.valor == valor)
+                    aux.prox = aux.prox.prox;
+                if (aux.valor == valor)
+                    removerNoFinal();
+            }
+        }
     }
 
     public void imprimir() {
